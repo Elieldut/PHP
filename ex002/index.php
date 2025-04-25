54e42e7ae3ddda26a6622a1afb5ba9ab27b16824
Elieldut <elielalves926@gmail.com>
2025-04-25 16:29:09 -0300

    Update index.php

--- ex002/index.php
+++ ex002/index.php
@@ -7,15 +7,14 @@
 </head>
 <body>
     <h1>Exemplo de php</h1>
+
     <?php
     // use a tag date_default_timezone_set para colocar a hora em fuso horario
     date_default_timezone_set("America/Sao_Paulo");
     echo "Hoje é dia " . date ("d/m/Y");
     echo "E a hora autal " . date("G:i:s");
     ?>
-    <br>
-    <br>
-   
+    <p></p>
     <?php
     $nome = "Eliel";
     $sobrenome = "Alves";
@@ -23,9 +22,13 @@
     const PAÍS = "Brasil";
     echo "Prazer meu nome é $nome $sobrenome! Moro no " . PAÍS;
     ?>
-<br>
     <?php 
     echo "e estou cursando $curso";
+    ?>
+    <p></p>
+    <?php
+    const _Q = "100";
+    echo "Me retorne esse número " . _Q;
     ?>
 
 </body>
