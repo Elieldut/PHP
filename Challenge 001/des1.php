00e6c8edff7fecae386c86adf21a5f0113f51e8c
Elieldut <elielalves926@gmail.com>
2025-05-10 18:31:21 -0300

    commit

--- Challenge 001/des1.php
+++ Challenge 001/des1.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Challenge 1</title>
     <link rel="stylesheet" href="style.css.css">
 </head>
 <body>
 <main>
-        <h1>Resultado Final</h1>
-        <?php
+    <h1>Resultado Final</h1>
+    <?php
 
             // O $n é uma variável como toda variável precisa de um valor vamos aplicar, Como? Utilizando o ($_REQUEST,_GET ou _POST...) o $_REQUEST é utilizado para atribuir um valor a variável, para isso ele utiliza o parâmetro ["num"] para pegar a chave no array, ou seja esse parâmetro vai ter quer ser o mesmo que você utiliza no seu 'name="num"' do input do fromulário. Agora para que serve essas duas interrogações? Essa duas '??' representa um operador de coalescência nula. Ele é utilizado com um 0 na frente, para que fins '??' é usado? Para indicar o valor que você colocar no ["num"], agora se esse ["num"] não estiver sendo usado no meu formulário o valor vai ser sempre 0 independente do valor que você colocar no formulário, no entanto esse mesmo ["num"] tem que está no 'name="num"' do formulário.
             $n = $_GET["num"] ?? 0;
---
+++ Challenge 002/Desafio.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Challenge 2</title>
+    <link rel="stylesheet" href="../Challenge 001/style.css.css">
+</head>
+<body>
+    <main>
+        <h1>Números Aleatórios</h1>
+        <?php
+        $min = 0;
+        $max = 100;
+        $num = mt_rand($min, $max);
+        echo "Gerando um Número Aleatório entre $min e $max... <br>O valor gerado foi <strong>$num</strong>";
+
+        ?>
+        <button onclick="javascript:document.location.reload()">&#x1F504; AGAIN</button>
+    </main>
+</body>
+</html>
